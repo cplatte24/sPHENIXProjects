67d2b9571fc58523e36e77153e64c39d1c17e17e
jamesjl3 <jennifer.l.james@vanderbilt.edu>
2023-05-22 14:00:53 -0400

    2nd commit of SectorMap_Display.C

--- SectorMap_Display.C
+++ SectorMap_Display.C
@@ -12,11 +12,11 @@
 #include <iostream>
 
 // forward declaration
-void Ana( TNtuple*, TNtuple*, char[2], double tot[24], double dead[24], int );
+void Ana( TNtuple*, TNtuple*, TNtuple*, char[2], double tot[24], double dead[24], int );
 
 void SectorMap_Display( )
 {
-  string runNumber = "pedestal-00010131";
+  string runNumber = "pedestal-00010305";
 
   // const string fileName = "/sphenix/user/rosstom/test/testFiles/TPC_ebdc"+sectorNumber+"_"+runNumber+"-0000.prdf_TPCRawDataTree.root";
 
@@ -33,10 +33,11 @@ void SectorMap_Display( )
 
   TH1F *widths_dist = new TH1F("widths_dist", "Pedestal Runs, Distribution of std. dev. ADC for each channel in TPC",100,-0.5,19.5);
   */
-  TFile h_outfile10131("pedestal-10131-outfile.root", "RECREATE");
+  TFile h_outfile10305("pedestal-10305-outfile.root", "RECREATE");
   TNtuple *h_Alive=new TNtuple("h_Alive","Location Tntuple Cuts","chan_id:fee_id:module_id:pedMean:pedStdi:sec_id");
   TNtuple *h_AliveTot=new TNtuple("h_AliveTot","Location Tntuple No Cuts","chan_id:fee_id:module_id:pedMean:pedStdi:sec_id");
- 
+  TNtuple *h_AliveNoise=new TNtuple("h_AliveNoise","Location Tntuple Cuts","chan_id:fee_id:module_id:pedMean:pedStdi:sec_id:noise");
+
  
 
   //create TNtuple to produce plots of sector_id:fee_id:channel_id
@@ -47,32 +48,25 @@ void SectorMap_Display( )
     char run[2]; //define character string
     std::sprintf(run,run_str[i]); //print run_str values indexed at i<72
 
-    Ana( h_Alive, h_AliveTot, run, tot, dead, i); //create Ana function
+    Ana( h_Alive, h_AliveTot,h_AliveNoise, run, tot, dead, i); //create Ana function
 
     tot_ult[i] += tot[i]; //find total channels
     dead_ult[i] += dead[i]; //find dead channels
   }
 
-  h_outfile10131.cd();
+  h_outfile10305.cd();
   h_Alive->Write();
   h_AliveTot->Write();
-  h_outfile10131.Close();
+  h_outfile10305.Close();
 
  }
 
 // Define Ana function 
-/*
-void Ana( TNtuple *ntup, TNtuple *ntuptup, TH1F *means_dist = new TH1F("means_dist", "Distribution of mean ADC for each channel in TPC",200,0,200), TH1F *widths_dist = new TH1F("widths_dist", "Distribution of std. dev. ADC for each channel in TPC",100,0,20), char run[6] = "175241",double tot[24]={0}, double dead[24]={0}, int j = 0)
-{ // Ana starts
-*/
-void Ana( TNtuple *Alive, TNtuple *AliveTot, char run[2] = "11",double tot[24]={0}, double dead[24]={0}, int j = 0)
+
+void Ana( TNtuple *Alive, TNtuple *AliveTot, TNtuple *AliveNoise,  char run[2] = "11",double tot[24]={0}, double dead[24]={0}, int j = 0)
 { // Ana starts     
      
-
-
-  const TString filename2( Form( "/sphenix/u/jamesj3j3/workfest_Charles_mistake/sPHENIXProjects/outputfile_TPC_ebdc%s_pedestal-00010131.root", run) );
-  // const string fileName = "( Form(/sphenix/user/rosstom/test/testFiles/TPC_ebdc"+sectorNumber+"_"+runNumber+"-0000.prdf_TPCRawDataTree.root) )";
-  //    std::cout << "Analyze - filename2: " << filename2 << std::endl;
+  const TString filename2( Form( "/sphenix/u/jamesj3j3/workfest_Charles_mistake/sPHENIXProjects/outputfile_TPC_ebdc%s_pedestal-00010305.root", run) );
 
   //TFile *infile2 = new TFile(filename2);
   TFile* infile2 = TFile::Open(filename2);
@@ -111,8 +105,10 @@ void Ana( TNtuple *Alive, TNtuple *AliveTot, char run[2] = "11",double tot[24]={
      //if(sec_reg_id.first != -1 && sec_reg_id.second != -1) ntuptup->Fill(1.0*sec_reg_id.first,1.0*(i%256),1.0*sec_reg_id.second);
       AliveTot->Fill(1.0*chan,1.0*fee,1.0*module,pedMean,pedStdi,1.0*sec);
 
-      if (isAlive == 1)Alive->Fill(1.0*chan,1.0*fee,1.0*module,pedMean, pedStdi, 1.0*sec); ///addd later
-
+      if (isAlive == 1){
+	Alive->Fill(1.0*chan,1.0*fee,1.0*module,pedMean, pedStdi, 1.0*sec); ///addd later
+	//	AliveNoise->Fill(1.0*chan,1.0*fee,1.0*module,pedMean, pedStdi, 1.0*sec, noise); ///addd later                                             
+      }
     //     std::pair<int,int> sec_reg_id = get_sec_reg_id(run, i);
 
    }
